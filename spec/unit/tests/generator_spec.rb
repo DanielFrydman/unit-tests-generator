5df1ec0942d1340e0de84d36b79ddaf194cb5c4f
Daniel Frydman <danielfrydman@id.uff.br>
2023-09-17 16:51:55 -0300

    First commit

---
+++ spec/unit/tests/generator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Unit::Tests::Generator do
+  it "has a version number" do
+    expect(Unit::Tests::Generator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
